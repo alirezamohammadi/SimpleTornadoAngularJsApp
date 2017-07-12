cc7d1b7d29138f78b8789e2f6114f1aaf4c82a89
Alireza Mohammadi <alireza_6313@yahoo.com>
2017-07-12 16:28:19 +0430

    templates path defined and index.html template added to project

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 import tornado.ioloop
 import tornado.web
+import sqlite3
+import json
+import os
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("index.html")
+
+
+class UsersList(tornado.web.RequestHandler):
+    def get(self):
+        query = "SELECT * FROM 'users';"
+        users = self.application.db.execute(query).fetchall()
+        list_json_users = []
+        for user in users:
+            json_user = {
+                "id": user[0],
+                "first_name": user[1],
+                "last_name": user[2],
+                "age": user[3]
+            }
+            list_json_users.append(json_user)
+
+        self.write(str(json.dumps(list_json_users)))
+
 
 def make_app():
+    settings = {
+        "static_path": os.path.join(os.path.dirname(__file__), "static"),
+        "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+    }
     return tornado.web.Application([
         (r"/", MainHandler),
-    ])
+        (r"/users", UsersList),
+    ], **settings)
+
 
 if __name__ == "__main__":
     app = make_app()
+    app.db = sqlite3.connect("data_base.db")
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
