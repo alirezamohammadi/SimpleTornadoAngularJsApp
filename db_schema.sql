09cac16a3dffde9523fb6164ce2c1b2a798d1555
Alireza Mohammadi <alireza_6313@yahoo.com>
2017-07-12 16:27:15 +0430

    some changes in data base schema and content

--- db_schema.sql
+++ db_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE 'users'(
     'id'            INTEGER PRIMARY KEY AUTOINCREMENT,
-    'first_name'    VARCHAR(50),
-    'last_name'     VARCHAR(50),
-    'age'           INTEGER
+    'first_name'    VARCHAR(50) NOT NULL,
+    'last_name'     VARCHAR(50) NOT NULL,
+    'age'           INTEGER     NOT NULL
 );
